02db9c0a91ccd8eafec352fad689ce56541cdb9f
Nikola <nikola.tomasovic@medieinstitutet.se>
2022-01-23 21:37:36 +0100

    lagt till meny och fungerande css

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
   // Copy images
   eleventyConfig.addPassthroughCopy("src/img");
+  // Coyp css-file
+  eleventyConfig.addPassthroughCopy("src/main.css");
 
   return {
     dir: {

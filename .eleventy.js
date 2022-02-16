5db43bb835134acbfe7a1f5674c9084f7cbd4682
Nikola <nikola.tomasovic@medieinstitutet.se>
2022-02-16 21:12:19 +0100

    implementerat scss compiler för fontawesome och lagt till footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,29 @@
+const sass = require("sass");
+
 module.exports = function (eleventyConfig) {
   // Copy images
   eleventyConfig.addPassthroughCopy("src/img");
-  // Coyp css-file
-  eleventyConfig.addPassthroughCopy("src/main.css");
+  // Copy Fontawesome
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/@fortawesome/fontawesome-free/webfonts": "webfonts",
+  });
+
+  // Compile Sass
+  eleventyConfig.addTemplateFormats("scss");
+
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+    compile: function (contents, inputPath) {
+      let includesPaths = [this.config.dir.includes, "./node_modules/"];
+      return (data) => {
+        let ret = sass.compile(inputPath, {
+          loadPaths: includesPaths,
+          style: "compressed",
+        });
+        return ret.css.toString("utf8");
+      };
+    },
+  });
 
   return {
     dir: {
